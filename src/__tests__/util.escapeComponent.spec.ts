4b791da42aea6ab65c2658cc99e04a48174af045
streamich <streamich@gmail.com>
2024-10-05 12:48:24 +0200

    feat: 🎸 add initial implementation

---
+++ src/__tests__/util.escapeComponent.spec.ts
@@ -0,0 +1,11 @@
+import {test, expect} from 'vitest';
+import {escapeComponent} from '../util';
+
+test('string without escaped characters as is', () => {
+  const res = escapeComponent('foobar');
+  expect(res).toBe('foobar');
+});
+
+test('replaces special characters', () => {
+  expect(escapeComponent('foo~/')).toBe('foo~0~1');
+});
